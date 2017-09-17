ec58e291e5a1e6aed02dfb6ef7657d934d6e8dbd
Alangi Derick <alangiderick@gmail.com>
2017-09-17 13:24:25 +0100

    sanalyze: Entry point
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

--- sanalyze.go
+++ sanalyze.go
@@ -2,4 +2,24 @@
 // LICENSE: MIT, all right reserved 2017
 // Description: Entry point for the SQL analyzer tool
 
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	for {
+		fmt.Printf("gosql-query-analyzer> ")
+
+		reader := bufio.NewReader(os.Stdin)
+
+		query, _ := reader.ReadString('\n')
+		fmt.Println("query: ", query)
+		// Enter Ctrl + C to quit.
+	}
+
+}
