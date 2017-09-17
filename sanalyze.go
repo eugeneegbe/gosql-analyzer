2cde29f8a5e0e4648903d8196ad416a2021909d5
Alangi Derick <alangiderick@gmail.com>
2017-09-17 13:45:58 +0100

    sanalyze: Clear screen functions on multiple platforms
    * Clear screen function on Linux and Windows platform has
      been implemented.
    
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

--- sanalyze.go
+++ sanalyze.go
@@ -8,9 +8,43 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
+	"time"
 )
 
+var clear map[string]func() // create a map for storing clear functions
+
+// functions to perform clear screen operations in linux and windows platform
+func init() {
+	clear = make(map[string]func()) // Initialize it
+	clear["linux"] = func() {
+		cmd := exec.Command("clear") // Linux systems
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cls") // Windows systems
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
+func ClearScreen() {
+	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+	if ok {                          // if we defined a clear func for that platform:
+		value() // we execute it
+	} else { // unsupported platform
+		panic("Your platform is unsupported so screen can't be cleared!")
+	}
+}
+
 func main() {
+
+	// Clear screen and prepare the analyzer console
+	ClearScreen()
+
+	fmt.Printfln("Welcome to the Go SQL Query Analyzer (GoSQA) version 1.0")
 
 	for {
 		fmt.Printf("gosql-query-analyzer> ")
